756b6881316e95430aeb331eb5b071311f8eedd1
dpprdgls <daps.douglas@gmail.com>
2023-11-12 17:14:34 -0800

    add file structure

--- server.js
+++ server.js
@@ -12,31 +12,32 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //route path for a get request that returns all notes from noteDb in json format
+
 app.get("/api/notes", (req, res) => {
   res.json(noteDb);
-  // console.log("GET");
-});
-
-app.post("/api/notes", (req, res) => {
-  let filePath = path.join(__dirname, "./db/db.json");
-  let newNote = req.body;
-
-  noteDb.push(newNote);
-  console.log(noteDb);
-
-  try {
-    fs.writeFileSync(filePath, JSON.stringify(noteDb), (err) => {
-      if (err) {
-        return console.log(err);
-      }
-      console.log("Posted note")
-    });
-  } catch (err) {
-    console.log(err);
-  }
-  res.json(newNote);
 });
 
 
+
+app.post('/api/notes', (req, res) => {
+    
+    let jsonFilePath= path.join(__dirname, "db/db.json");
+    let newNote = req.body;
+
+    noteDb.push(newNote);
+    console.log(noteDb);
+
+    try{
+        fs.writeFileSync(jsonFilePath, JSON.stringify(noteDb), (err) => {
+            if(err) {
+                return console.log(err);
+            }
+            console.log('Note is in database')
+        })
+    }catch(err) {
+        console.log(err);
+    }
+    res.json(newNote);
+})
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}.`));
