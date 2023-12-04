ef2a794ffee5ed400449241d0da35465ffea6229
Colin Douglas <cdouglas@tesla.com>
2023-12-04 07:16:00 -0800

    fixed delete note logic

--- controllers/deleteNote.js
+++ controllers/deleteNote.js
@@ -6,16 +6,13 @@ const { readFromFile, writeToFile } = require('../helpers/fsUtils');
 
 
 
-
 const deleteNote =  async (req, res) => {
     try {
         const deleteId = req.params.id;
-        // const notesData = await readFromFile(path.join(__dirname, "../db/db.json"));
-        // const existingNotes = JSON.parse(notesData);
         const existingNotes = await readFromFile(path.join(__dirname, "../db/db.json")).then((data) => JSON.parse(data));
         const indexToDelete = existingNotes.findIndex((note) => note.id === deleteId);
 
-        if (indexToDelete === -1) {
+        if (indexToDelete !== -1) {
             existingNotes.splice(indexToDelete, 1);
             writeToFile(path.join(__dirname, "../db/db.json"), existingNotes);
 
