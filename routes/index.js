189a0b601da26a7de9ad20bda2102baa42197df7
Colin Douglas <cdouglas@tesla.com>
2023-12-04 08:00:36 -0800

    move file

--- routes/index.js
+++ routes/index.js
@@ -9,20 +9,20 @@ router.use('/api', apiRoutes);
 //route to send the index file
 router.get('/', (req, res) => {
     console.log('Index request received');
-    res.sendFile(path.join(__dirname, '../index.html'));
+    res.sendFile(path.join(__dirname, '../public/assets/index.html'));
 });
 
 //route to send the notes file
 router.get('/notes', (req, res) => {
     console.log('Notes request received');
-    res.sendFile(path.join(__dirname, "../notes.html"));
+    res.sendFile(path.join(__dirname, "../public/assets/notes.html"));
 });
 
 //route to send the index file if wildcard requested
 
 router.get(/^(?!\/notes).*$/, (req, res) => {
     console.log('You are wild!');
-    res.sendFile(path.join(__dirname, "../index.html"));
+    res.sendFile(path.join(__dirname, "../public/assets/index.html"));
 });
 
 module.exports = router;
