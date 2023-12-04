13b89352c1eb9471dd9c015ba89820c8677521f6
Colin Douglas <cdouglas@tesla.com>
2023-12-04 07:16:53 -0800

    clean up

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -5,17 +5,6 @@ const noteRoutes = require('./noteRoutes');
 
 router.use('/notes', noteRoutes);
 
-// router.get('/', async (req, res) => {
-//     try {
-//         const notesData = await readFromFile(path.join, "../db/db.json");
-//         const notes = JSON.parse(notesData);
-//         res.json(notes);
-//     } catch (error) {
-//         console.error(error);
-//         res.status(500).json({error: 'Internal Server Error' });
-//     }
-// });
-
 
 
 
